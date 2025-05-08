396bb42a9e8451ab4ff75bb4d3026d51ef4f909b
Sao Bran Aung <saobran.aung61@gmail.com>
2025-05-08 21:38:53 +0630

    refactor: simplify RefreshToken property declaration in RefrehTokenRequestDTO

--- Controllers/Auth/Request/RefrehTokenRequestDTO.cs
+++ Controllers/Auth/Request/RefrehTokenRequestDTO.cs
@@ -1,14 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace BackendCustoms.Controllers.Auth.Request
 {
     public class RefrehTokenRequestDTO
     {
-        [Required]
-        public string RefreshToken { get; set; }
+        public required string RefreshToken { get; set; }
     }
 }
