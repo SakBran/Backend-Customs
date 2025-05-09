31bbd52dc11b6db0f029ad96283df0ff42f2991a
Sao Bran Aung <saobran.aung61@gmail.com>
2025-05-09 20:34:50 +0630

    feat: update cpde fole for setting case

---
+++ Migrations/20250509140424_authCase.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class authCase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ceirPortalName",
+                table: "systemSettings",
+                newName: "PaymentConfirmationURL_CEIR");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AuthorizationTokenURL_CEIR",
+                table: "systemSettings",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AuthorizationTokenURL_CEIR",
+                table: "systemSettings");
+
+            migrationBuilder.RenameColumn(
+                name: "PaymentConfirmationURL_CEIR",
+                table: "systemSettings",
+                newName: "ceirPortalName");
+        }
+    }
+}
