d6dbf798080f7b379baacc9371ea1f7258c65489
Sao Bran Aung <saobran.aung61@gmail.com>
2025-05-11 21:47:13 +0630

    fix: update code for send resend code

--- Controllers/CustomsData/CustomsDataController.cs
+++ Controllers/CustomsData/CustomsDataController.cs
@@ -7,6 +7,7 @@ using API.DBContext;
 using API.Model;
 using BackendCustoms.Controllers.CustomsData.Request;
 using BackendCustoms.DepedencyInjections.Interface;
+using BackendCustoms.DepedencyInjections.Service.CeirService.Request;
 using BackendCustoms.Model;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
@@ -23,12 +24,19 @@ namespace BackendCustoms.Controllers
         private readonly ApplicationDbContext _context;
         private readonly IHttpContextAccessor _httpContextAccessor;
         private readonly IGetaccessTokenService _getToken;
-
-        public CustomsDataController(ApplicationDbContext context, IHttpContextAccessor httpContextAccessor, IGetaccessTokenService getToken)
+        private readonly IGetSystemSetting _sys;
+        private readonly ICEIR_API_Service _irdService;
+        private readonly ICustomDataFilterAndSaveService _filterAndSaveService;
+        public CustomsDataController(ApplicationDbContext context,
+         IHttpContextAccessor httpContextAccessor, IGetaccessTokenService getToken, IGetSystemSetting sys,
+         ICEIR_API_Service irdService, ICustomDataFilterAndSaveService filterAndSaveService)
         {
             _context = context;
             _httpContextAccessor = httpContextAccessor;
             _getToken = getToken;
+            _sys = sys;
+            _irdService = irdService;
+            _filterAndSaveService = filterAndSaveService;
         }
 
         [HttpGet]
@@ -243,6 +251,81 @@ namespace BackendCustoms.Controllers
                 temp.EditById = user?.Id;
                 await _context.SaveChangesAsync();
                 return Ok(temp);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Send(SendRequest request)
+        {
+
+            var data = await _context.CustomsDatas.Where(x => x.id == request.id).FirstOrDefaultAsync();
+            if (data != null)
+            {
+                var user = new User();
+                var RequestToken = await _context.TokenModels.Where(x => x.Token == _getToken.GetaccessTokenAsync()).FirstOrDefaultAsync();
+                if (RequestToken != null)
+                {
+                    // Resend User case
+                    user = await _context.Users.Where(x => x.Id == RequestToken.UserId).FirstOrDefaultAsync();
+                }
+                string? token = null;
+                var setting = await _sys.GetAsync();
+                try
+                {
+                    #region Token Request
+                    var tokenRequest = new GetTokenRequest
+                    {
+                        authUrl = setting.AuthorizationTokenURL_CEIR,
+                        principal = setting.principal,
+                        credentials = setting.credentials
+                    };
+
+                    token = await _irdService.GetTokenAsync(tokenRequest);
+                    #endregion
+                }
+                catch (Exception ex)
+                {
+                    await _filterAndSaveService.SaveAccordingToStatus(data, HttpStatusCode.InternalServerError.ToString());
+                    return BadRequest();
+                }
+
+                #region Send to IRD and Update Success/Fail Status on CustomData
+                if (token != null)
+                {
+                    var confirmRequest = new ConfirmationRequest
+                    {
+                        body = new PaymentConfirmationRequest
+                        {
+                            CeirId = data.CEIRID,
+                            ReleaseOrderNumber = data.RONo,
+                            DateTime = data.RODate,
+                            SumCT = data.CT,
+                            SumCD = data.CD,
+                            SumAIT = data.AT,
+                            SumRF = data.RF
+                        },
+                        ApiURl = setting.PaymentConfirmationURL_CEIR,
+                        Token = token
+                    };
+                    var status = await _irdService.PaymentConfirmation(confirmRequest);
+                    await _filterAndSaveService.SaveAccordingToStatus(data, status);
+                    if (status == HttpStatusCode.OK.ToString())
+                    {
+                        return Ok(data);
+                    }
+                    else
+                    {
+                        return StatusCode(500, new { message = "An error occurred while updating data." });
+                    }
+                }
+                #endregion
+                data.SentDatetime = DateTime.Now;
+                await _context.SaveChangesAsync();
+                return Ok(data);
             }
             else
             {
---
+++ Controllers/CustomsData/Request/SendRequest.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackendCustoms.Controllers.CustomsData.Request
+{
+    public class SendRequest
+    {
+        public string? id { get; set; }
+    }
+}
