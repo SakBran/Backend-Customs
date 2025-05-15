deec422a1c32292a9cd3cda0f0ad39292d07c5cb
Sao Bran Aung <saobran.aung61@gmail.com>
2025-05-15 11:17:04 +0630

    feat: add delete list logs

--- Controllers/CeirdFromIRDController.cs
+++ Controllers/CeirdFromIRDController.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.Controllers;
 using API.DBContext;
+using API.Model;
 using BackendCustoms.Model;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BackendCustoms.Controllers
 {
@@ -17,6 +19,58 @@ namespace BackendCustoms.Controllers
         public CeirdFromIRDController(ApplicationDbContext context) : base(context)
         {
             _context = context;
+        }
+
+        [HttpGet]
+        [Route("Filter")]
+        public async Task<ActionResult<ApiResult<CeiridFromIRD>>> SentList(
+                        DateTime? receivedDatetimeFrom,
+                        DateTime? receivedDatetimeTo,
+                        DateTime? SentDateFrom,
+                        DateTime? SentDateTo,
+                        string? CeirId,
+                        bool? isSent,
+                        int pageIndex = 0,
+                        int pageSize = 10,
+                        string? sortColumn = null,
+                        string? sortOrder = null,
+                        string? filterColumn = null,
+                        string? filterQuery = null)
+        {
+            var query = _context.ceiridFromIRDs.AsNoTracking();
+            if (SentDateFrom != null)
+            {
+                query = query.Where(x => x.SendDatetime >= SentDateFrom);
+            }
+            if (SentDateTo != null)
+            {
+                query = query.Where(x => x.SendDatetime <= SentDateTo);
+            }
+            if (receivedDatetimeFrom != null)
+            {
+                query = query.Where(x => x.ReceivedDatetime >= receivedDatetimeFrom);
+            }
+            if (receivedDatetimeTo != null)
+            {
+                query = query.Where(x => x.ReceivedDatetime <= receivedDatetimeTo);
+            }
+            if (!String.IsNullOrEmpty(CeirId))
+            {
+                query = query.Where(x => x.CEIRID == CeirId);
+            }
+            if (isSent != null)
+            {
+                query = query.Where(x => x.IsSent == isSent);
+            }
+
+            return await ApiResult<CeiridFromIRD>.CreateAsync(
+                    query,
+                    pageIndex,
+                    pageSize,
+                    sortColumn,
+                    sortOrder,
+                    filterColumn,
+                    filterQuery);
         }
     }
 }
--- Controllers/DeleteLogsController.cs
+++ Controllers/DeleteLogsController.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.Controllers;
 using API.DBContext;
+using API.Model;
 using BackendCustoms.Model;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BackendCustoms.Controllers
 {
@@ -17,6 +19,58 @@ namespace BackendCustoms.Controllers
         public DeleteLogsController(ApplicationDbContext context) : base(context)
         {
             _context = context;
+        }
+
+        [HttpGet]
+        [Route("Filter")]
+        public async Task<ActionResult<ApiResult<CeiridFromIRD_DeletedLog>>> SentList(
+                        DateTime? fromDate,
+                        DateTime? toDate,
+                        DateTime? SentDateFrom,
+                        DateTime? SentDateTo,
+                        string? CeirId,
+                        bool? isSent,
+                        int pageIndex = 0,
+                        int pageSize = 10,
+                        string? sortColumn = null,
+                        string? sortOrder = null,
+                        string? filterColumn = null,
+                        string? filterQuery = null)
+        {
+            var query = _context.ceiridFromIRD_DeletedLogs.AsNoTracking();
+            if (SentDateFrom != null)
+            {
+                query = query.Where(x => x.SendDatetime >= SentDateFrom);
+            }
+            if (SentDateTo != null)
+            {
+                query = query.Where(x => x.SendDatetime <= SentDateTo);
+            }
+            if (fromDate != null)
+            {
+                query = query.Where(x => x.DeletedDateTime >= fromDate);
+            }
+            if (toDate != null)
+            {
+                query = query.Where(x => x.DeletedDateTime <= toDate);
+            }
+            if (!String.IsNullOrEmpty(CeirId))
+            {
+                query = query.Where(x => x.CEIRID == CeirId);
+            }
+            if (isSent != null)
+            {
+                query = query.Where(x => x.IsSent == isSent);
+            }
+
+            return await ApiResult<CeiridFromIRD_DeletedLog>.CreateAsync(
+                    query,
+                    pageIndex,
+                    pageSize,
+                    sortColumn,
+                    sortOrder,
+                    filterColumn,
+                    filterQuery);
         }
     }
 }
