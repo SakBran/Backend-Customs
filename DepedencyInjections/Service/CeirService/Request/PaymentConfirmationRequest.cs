68183cf1f23a2b281363fe43f417cb315a90d4fb
Sao Bran Aung <saobran.aung61@gmail.com>
2025-08-10 14:33:22 +0630

    feat: update code fix for payment confirmation case

--- DepedencyInjections/Service/CeirService/Request/PaymentConfirmationRequest.cs
+++ DepedencyInjections/Service/CeirService/Request/PaymentConfirmationRequest.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace BackendCustoms.DepedencyInjections.Service.CeirService.Request
 {
@@ -11,30 +12,37 @@ namespace BackendCustoms.DepedencyInjections.Service.CeirService.Request
     {
 
         [JsonPropertyName("ceirId")]
+        [JsonProperty("ceirId")]
         public string? CeirId { get; set; }
 
 
         [JsonPropertyName("releaseOrderNumber")]
+        [JsonProperty("releaseOrderNumber")]
         public string? ReleaseOrderNumber { get; set; }
 
 
         [JsonPropertyName("dateTime")]
+        [JsonProperty("dateTime")]
         public string? DateTime { get; set; }
 
 
         [JsonPropertyName("SUM_CT")]
+        [JsonProperty("SUM_CT")]
         public decimal? SumCT { get; set; } // Commercial Tax
 
 
         [JsonPropertyName("SUM_CD")]
+        [JsonProperty("SUM_CD")]
         public decimal? SumCD { get; set; } // Customs Duty
 
 
         [JsonPropertyName("SUM_AIT")]
+        [JsonProperty("SUM_AIT")]
         public decimal? SumAIT { get; set; } // Advanced Income Tax
 
 
         [JsonPropertyName("SUM_RF")]
+        [JsonProperty("SUM_RF")]
         public decimal? SumRF { get; set; } // Penalties
     }
 }
